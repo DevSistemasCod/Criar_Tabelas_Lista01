edd0b45b09d1e5247da76f0ce4c608235ba051f1
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-09-03 20:01:20 -0300

    Update exerc_03_SQL.sql

--- exerc_03_SQL.sql
+++ exerc_03_SQL.sql
@@ -1,6 +1,6 @@
 -- Tabela Disciplina
 CREATE TABLE disciplina (
-    codigo_disciplina INT PRIMARY KEY,
+    codigo_disciplina VARCHAR(15) PRIMARY KEY,
     nome_disciplina VARCHAR(50),
     carga_horaria INT,
     descricao VARCHAR(100)
@@ -9,7 +9,7 @@ CREATE TABLE disciplina (
 -- Tabela Curso
 CREATE TABLE curso (
     sigla_curso VARCHAR(10) PRIMARY KEY,
-    codigo_disciplina INT,
+    codigo_disciplina VARCHAR(15),
     nome VARCHAR(50),
     carga_horaria INT,
     descricao VARCHAR(100),
@@ -18,19 +18,19 @@ CREATE TABLE curso (
 
 -- Tabela Professor
 CREATE TABLE professor (
-    nro_registro_professor INT PRIMARY KEY,
+    nro_registro_professor VARCHAR(15) PRIMARY KEY,
     nome VARCHAR(50),
-    codigo_turma INT,
-    codigo_disciplina INT,
+    codigo_turma VARCHAR(15),
+    codigo_disciplina VARCHAR(15),
     FOREIGN KEY (codigo_disciplina) REFERENCES disciplina(codigo_disciplina)
 );
 
 -- Tabela Turma
 CREATE TABLE turma (
-    codigo_turma INT PRIMARY KEY,
+    codigo_turma VARCHAR(15) PRIMARY KEY,
     nro_alunos INT,
     sigla_curso VARCHAR(10),
-    nro_registro_Professor INT,
+    nro_registro_Professor VARCHAR(15),
     periodo VARCHAR(20),
     FOREIGN KEY (sigla_curso) REFERENCES curso(sigla_curso),
     FOREIGN KEY (nro_registro_Professor) REFERENCES professor(nro_registro_professor)
@@ -40,7 +40,7 @@ CREATE TABLE turma (
 CREATE TABLE aluno (
     nro_matricula INT PRIMARY KEY,
     nome_aluno VARCHAR(50),
-    codigo_turma INT,
+    codigo_turma VARCHAR(15),
     FOREIGN KEY (codigo_turma) REFERENCES turma(codigo_turma)
 );
 
@@ -49,7 +49,7 @@ CREATE TABLE matricula (
     nro_matricula INT PRIMARY KEY,
     nome_aluno VARCHAR(50),
     sigla_curso VARCHAR(10),
-    ano INT,
+    data_matricula DATE NOT NULL,
     semestre INT,
     FOREIGN KEY (sigla_curso) REFERENCES curso(sigla_curso),
 	FOREIGN KEY (nro_matricula) REFERENCES aluno(nro_matricula)
